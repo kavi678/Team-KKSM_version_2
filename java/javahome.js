46d111e4ca60cbb896a26c22c246ad4bdde3d86e
kavi678 <kavipatel111.kp@gmail.com>
2019-04-30 13:42:14 +0930

    adding

---
+++ java/javahome.js
@@ -0,0 +1,27 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+window.onscroll = function() {myFunction()};
+
+var lnav = document.getElementById("lnav");
+var sticky = lnav.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    lnav.classList.add("sticky")
+  } else {
+    lnav.classList.remove("sticky");
+  }
+}
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
